73b3e37ea376edd095b86bdab8d1db9273d197a9
Zac Smart <zac_chaz@outlook.com>
2022-10-31 16:16:13 +1100

    get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../Develop/models');
 // The `/api/products` endpoint
 
 // get all products
+ // be sure to include its associated Category and Tag data
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{
+      model: Category
+    },
+    {
+      model: Tag,
+      through: ProductTag,
+      as: 'tags'
+    
+    }]
+  })
+  .then(data => res.json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
